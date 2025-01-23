357d05d633074655700834d7a11802f4c12b9bd3
Fabian lenga <fabilenga@gmail.com>
2025-01-23 20:18:15 +0200

    Update login_main_page.py
    Change some locators.

--- HomeTask/login_main_page.py
+++ HomeTask/login_main_page.py
@@ -1,21 +1,30 @@
-# import allure
+from logger import logger
 
 
 class Login_page_main:
+    EMAIL_INPUT = "input[id=':r1:']"
+    EMAIL_ERROR_MESSAGE = "//p[@id=':r1:-helper-text']"
+    PASSWORD_INPUT = "input[placeholder='Password']"
+    PASSWORD_ERROR_MESSAGE = "//p[@id=':r2:-helper-text']"
+    LOGIN_BUTTON = "//span[normalize-space()='Login']"
+    ERROR_DIALOG = "Sign in Failed -"
+
     def __init__(self, page):
         self.page = page
-        self.email_address = self.page.locator("input[id=':r1:']")
-        self.email_address_error_message = self.page.locator("//p[@id=':r1:-helper-text']")
-        self.password = self.page.locator("input[placeholder='Password']")
-        self.password_error_message = self.page.locator("/p[@id=':r2:-helper-text']")
-        self.login_button = self.page.locator("//span[normalize-space()='Login']")
-        # self.log_in_error_dialog = self.page.loctor("Sign in Failed -")
+        self.email_address = self.page.locator(self.EMAIL_INPUT)
+        self.email_address_error_message = self.page.locator(self.EMAIL_ERROR_MESSAGE)
+        self.password = self.page.locator(self.PASSWORD_INPUT)
+        self.password_error_message = self.page.locator(self.PASSWORD_ERROR_MESSAGE)
+        self.login_button = self.page.locator(self.LOGIN_BUTTON)
 
     def fill_email_address(self, email):
+        logger.info(f"Filling email address: {email}")
         self.email_address.fill(email)
 
     def type_password(self, textarea):
+        logger.info("Typing password.")
         self.password.fill(textarea)
 
     def submit_button(self):
+        logger.info("Submitting login button.")
         self.login_button.click()
