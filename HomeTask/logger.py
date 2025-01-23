badcc6e334b3e05fd1d14b22071b71b8a726ff50
Fabian lenga <fabilenga@gmail.com>
2025-01-23 20:18:56 +0200

    Update logger.py

--- HomeTask/logger.py
+++ HomeTask/logger.py
@@ -1,16 +1,13 @@
 import logging
-import os
+
+logger = logging.getLogger("test_logger")
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_formatter = logging.Formatter("[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s")
+console_handler.setFormatter(console_formatter)
+logger.addHandler(console_handler)
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    filename="/Users/Fabian/Documents/Home_Task.log",
-    format="[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
-_logger = logging.getLogger()
-_logger.setLevel(os.getenv("LOG_LEVEL", logging.INFO))
-
-
-def logger():
-    return _logger
+def get_logger():
+    return logger
